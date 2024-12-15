09ebdae463556f807bca8432653570f3a4e7b428
sszaf <127353106+sszaf@users.noreply.github.com>
2024-12-15 18:44:08 +0100

    Update SecurityConfig.java

--- RestApi/src/main/java/com/socialtripper/restapi/config/SecurityConfig.java
+++ RestApi/src/main/java/com/socialtripper/restapi/config/SecurityConfig.java
@@ -11,45 +11,45 @@ import org.springframework.security.web.SecurityFilterChain;
 /**
  * Konfiguracja zabezpieczeń endpointów przy pomocy tokenów jwt z AWS Cognito.
  */
-//@Configuration
+@Configuration
 public class SecurityConfig {
     /**
      * region aws
      */
-//    @Value("${cognito.region}")
-//    private String region;
-//
+   @Value("${cognito.region}")
+   private String region;
+
     /**
      * identyfikator user poola AWS Cognito
      */
-//    @Value("${cognito.userPoolId}")
-//    private String userPoolId;
-//
+   @Value("${cognito.userPoolId}")
+   private String userPoolId;
+
     /**
      * Metoda definiująca reguły dostępu do endpointów - filter chain.
      */
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests((authorizeRequests) -> authorizeRequests
-//                        .requestMatchers("/**").permitAll()
-//                        .anyRequest().authenticated()
-//                );
-////                .oauth2ResourceServer(oauth2 -> oauth2
-////                        .jwt(jwt -> jwt.decoder(jwtDecoder()))
-////                );
-//        return http.build();
-//    }
-//
+   @Bean
+   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+       http
+               .authorizeHttpRequests((authorizeRequests) -> authorizeRequests
+                       .requestMatchers("/**").permitAll()
+                       .anyRequest().authenticated()
+               );
+               .oauth2ResourceServer(oauth2 -> oauth2
+                       .jwt(jwt -> jwt.decoder(jwtDecoder()))
+               );
+       return http.build();
+   }
+
     /**
      * Metoda walidująca poprawność tokenów.
      */
-//    @Bean
-//    public JwtDecoder jwtDecoder() {
-//        String jwkSetUri = String.format(
-//                "https://cognito-idp.%s.amazonaws.com/%s/.well-known/jwks.json",
-//                region,
-//                userPoolId);
-//        return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
-//    }
+   @Bean
+   public JwtDecoder jwtDecoder() {
+       String jwkSetUri = String.format(
+               "https://cognito-idp.%s.amazonaws.com/%s/.well-known/jwks.json",
+               region,
+               userPoolId);
+       return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
+   }
 }
